d0f00da99444895e84ca7d90d55c0f62af283cc4
11344153 <yiting.3211@email.com>
2026-05-04 18:43:34 +0800

    feat: landmark detail

--- Landmark/ContentView.swift
+++ Landmark/ContentView.swift
@@ -5,17 +5,42 @@
 //  Created by 11344153 on 2026/5/4.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // 顯示地圖
+            MapView()
+                .frame(height: 300)
+            
+            // 顯示圓形圖片並向上位移，使其重疊在地圖邊緣
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                // 標題文字
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    // 副標題文字
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    // 地區文字
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary) // 讓副標題顏色稍微淡一點，更具層次感
+            }
+            .padding()
+            
+            Spacer() // 將所有內容往上推
         }
-        .padding()
     }
 }
 
---
+++ Landmark/MapView.swift
@@ -0,0 +1,30 @@
+//
+//  MapView.swift
+//  Landmark
+//
+//  Created by 11344153 on 2026/5/4.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    //24.957134675191725, 121.24361656965657
+    var center = CLLocationCoordinate2D(latitude: 24.957134675191725, longitude: 121.24361656965657)
+    
+    var span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+    
+    var body: some View {
+        
+        Map(initialPosition: .region(
+            MKCoordinateRegion(
+                center: center,
+                span: span
+            )
+        ))
+    }
+}
+
+#Preview {
+    MapView()
+}
